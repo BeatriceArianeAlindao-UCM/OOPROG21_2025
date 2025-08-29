8ff4547e001fdfcceb9aec16dd63c5f83abb2fd0
AlindaoBeatriceAriane <beatriceariane38@gmail.com>
2025-08-29 10:49:59 +0800

    Add files via upload

---
+++ chapter_1/Comments.java
@@ -0,0 +1,24 @@
+package JAVAUC;
+
+public class Comments {
+
+	public static void main(String[] args) {
+		
+		// This is a comment. Use for tracking also
+		
+		/* This
+		 * is
+		 * a
+		 * Multi-line comment
+		 * for
+		 * tracking
+		 * 
+		 */
+		
+		/**
+		 * This is a documentation comment
+		 * This will be use to identify the process of the program/system
+		 */
+	}
+
+}
---
+++ chapter_1/FavoriteSong.java
@@ -0,0 +1,13 @@
+package JAVAUC;
+
+public class FavoriteSong {
+
+	public static void main(String[] args) {
+	System.out.println("So I sneak out to the garden to see you\r\n"
+			+ "We keep quiet, 'cause we're dead if they knew\r\n"
+			+ "So close your eyes\r\n"
+			+ "Escape this town for a little while, oh oh");
+
+	}
+
+}
